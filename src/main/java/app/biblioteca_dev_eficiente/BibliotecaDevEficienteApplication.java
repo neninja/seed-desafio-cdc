b6856647df7c5fa0f8c16a5eda44bfee5fa5734b
Neni <felipe.dacs@gmail.com>
2026-02-10 20:39:05 -0300

    initial commit
    - spring web
    - data jpa
    - h2 database

---
+++ src/main/java/app/biblioteca_dev_eficiente/BibliotecaDevEficienteApplication.java
@@ -0,0 +1,13 @@
+package app.biblioteca_dev_eficiente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaDevEficienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaDevEficienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/biblioteca_dev_eficiente/BibliotecaDevEficienteApplicationTests.java
@@ -0,0 +1,13 @@
+package app.biblioteca_dev_eficiente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaDevEficienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
